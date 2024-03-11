62f8d81e1ff8c580ed6aa61458c4c6cd4cc4a555
Viktor Immanuel Calonia <>
2024-03-11 09:25:26 +0800

    Add xcodebuild

--- .config/nvim.nvchad.custom.config/plugins.lua
+++ .config/nvim.nvchad.custom.config/plugins.lua
@@ -51,6 +51,20 @@ local plugins = {
         "codelldb",
       }
     }
+  },
+  {
+    "wojciech-kulik/xcodebuild.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-tree.lua", -- if you want the integration with file tree
+    },
+    config = function()
+      require("xcodebuild").setup({
+        -- put some options here or leave it empty to use default settings
+      })
+    end,
   }
 }
 return plugins
