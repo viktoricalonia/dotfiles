3375476ac1900714336e898ab2ff78b2e0fa4d54
Viktor Immanuel Calonia <viktoricalonia@gmail.com>
2025-02-20 09:33:49 +0800

    Add gen xcode compilation database

--- .local/scripts/focus.sh
+++ .local/scripts/focus.sh
@@ -1,43 +1,8 @@
 #!/bin/bash
-
+MINWAIT=10
+MAXWAIT=30
 while [ 1 ];
 do
-  sleep 20;
-  MAXCOUNT=${RANDOM:0:2}
-  count=1
-  while [ "$count" -le $MAXCOUNT ]      # Generate 10 ($MAXCOUNT) random integers.
-  do
-    cliclick m:${RANDOM:0:3},${RANDOM:0:3};
-    sleep ${RANDOM:0:1};
-    cliclick m:${RANDOM:0:3},${RANDOM:0:3};
-    let "count += 1"  # Increment count.
-  done
-  
-  sleep 10;
-  MAXCOUNT=${RANDOM:0:2}
-  count=1
-  while [ "$count" -le $MAXCOUNT ]      # Generate 10 ($MAXCOUNT) random integers.
-  do
-    cliclick m:${RANDOM:0:3},${RANDOM:0:3};
-    sleep ${RANDOM:0:1};
-    cliclick m:${RANDOM:0:3},${RANDOM:0:3};
-    let "count += 1"  # Increment count.
-  done
-
-  MAXCOUNT=${RANDOM:0:1}
-    count=1
-    while [ "$count" -le $MAXCOUNT ]     # Generate 10 ($MAXCOUNT) random integers.
-    do
-      cliclick m:${RANDOM:0:3},${RANDOM:0:3};
-      sleep ${RANDOM:0:1};
-      cliclick m:${RANDOM:0:3},${RANDOM:0:3};
-      sleep ${RANDOM:0:1};
-      cliclick m:${RANDOM:0:3},${RANDOM:0:3};      
-      sleep ${RANDOM:0:1};
-      cliclick m:${RANDOM:0:3},${RANDOM:0:3};      
-      sleep ${RANDOM:0:1};
-      cliclick m:${RANDOM:0:3},${RANDOM:0:3};      
-      sleep ${RANDOM:0:1};
-      let "count += 1"; # Increment count.
-    done
+  sleep $((MINWAIT+RANDOM % (MAXWAIT-MINWAIT)))
+  cliclick m:${RANDOM:0:3},${RANDOM:0:3};      
 done
