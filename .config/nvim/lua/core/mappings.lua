efce23d6565009f5ef717cffa6214277aab41ccc
Viktor Immanuel Calonia <viktoricalonia@gmail.com>
2025-09-25 13:25:26 +0800

    Add quickfix mappints

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -25,7 +25,25 @@ M.general = {
     ["<C-k>"] = { "<C-w>k", "Window up" },
 
     -- quick fix
-    ["<C-q>"] = { "<cmd>copen <CR>", "Open Quick fix" },
+    ["<C-q>"] = {
+      function()
+        local qf_exists = false
+        for _, win in pairs(vim.fn.getwininfo()) do
+          if win["quickfix"] == 1 then
+            qf_exists = true
+          end
+        end
+        if qf_exists == true then
+          vim.cmd "cclose"
+          return
+        end
+        if not vim.tbl_isempty(vim.fn.getqflist()) then
+          vim.cmd "copen"
+        end
+      end,
+      "Toggle Quickfix window"
+    },
+
 
     -- save
     ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
