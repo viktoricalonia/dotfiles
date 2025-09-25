9322a6957cbc9098199ee665b4e3701459a77ffc
Viktor Immanuel Calonia <viktoricalonia@gmail.com>
2025-09-25 13:27:35 +0800

    Add quickfix mapping

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -3,6 +3,26 @@ local M = {}
 M.dap = {
   plugin = true,
   n = {
+    -- quick fix
+    ["<C-q>"] = {
+      function()
+        local qf_exists = false
+        for _, win in pairs(vim.fn.getwininfo()) do
+          if win["quickfix"] == 1 then
+            qf_exists = true
+          end
+        end
+        if qf_exists == true then
+          vim.cmd "cclose"
+          return
+        end
+        if not vim.tbl_isempty(vim.fn.getqflist()) then
+          vim.cmd "copen"
+        end
+      end,
+      "Open Quick fix"
+    },
+
     -- DAP
     ["<leader>db"] = {
       "<cmd> DapToggleBreakpoint <CR>",
